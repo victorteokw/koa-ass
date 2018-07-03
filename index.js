3214d2201f0ba6c697e238678297ba2672458d12
Zhang Kaiyu <yeannylam@gmail.com>
2018-07-03 15:42:16 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function(values) {
   return async function(ctx, next) {
-    const clashedKeys = Object.keys(values).filter((v) => ctx.v !== undefined);
+    const clashedKeys = Object.keys(values).filter((k) => ctx[k] !== undefined);
     clashedKeys.forEach((k) => {
       console.log(`[KOA-ASS] Warning: '${k}' is defined on context.`);
     });
